13283c30158895c8d0eacacade5e5e5b2e652bea
Ajay Kumar Yadav <32960809+ajaydeveloper007@users.noreply.github.com>
2023-01-03 09:00:06 +0530

    Add files via upload

---
+++ EmployeeDetail.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+
+  
+public class EmployeeDetail {
+    long employeeid;
+    String employeeName;
+    long managerid;
+    long hrld;
+    char gender;
+    int annualLeave ;
+    int paternityLeave;
+    
+}
